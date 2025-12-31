e3722237686b2fbf7bac78c1c647828971b8d61b
ilsyafiq <syfiqiikhwan@gmail.com>
2025-12-31 13:06:14 +0800

    Lab 3 - Done until Exercise 3

--- labs/lab03/exercise1/exercise1.py
+++ labs/lab03/exercise1/exercise1.py
@@ -1,14 +1,19 @@
 pixels = [100, 120, 200, 150, 180, 160, 140]
 
 def count_bright_spots(pixels):
+    count = 0
     for i in range(len(pixels)):
-        if i == 0 or 6:
+        if i == 0 or i == len(pixels)-1:
             continue
         else:
             if pixels[i] > pixels[i+1] and pixels[i] > pixels[i-1]:
-                print "Bright spot"
+                count += 1
             else:
-                print "Not a bright spot"
+                continue
+    
+    return count
+
+print(count_bright_spots([100, 120, 200, 150, 180, 160, 140]))
 
 
 
--- labs/lab03/exercise2/exercise2.py
+++ labs/lab03/exercise2/exercise2.py
@@ -1,5 +1,18 @@
 def find_station(stations, name):
-    pass
+    for i in range(len(stations)):
+        if stations[i] == name:
+            return i
+    
+    return 
 
 def count_stops(stations, start, stop):
-    pass
+    start_index = find_station(stations, start)
+    stop_index = find_station(stations, stop)
+    if start_index == None or stop_index == None:
+        return -1
+    else:
+        if start_index < stop_index:
+            return stop_index - start_index
+        else:
+            return start_index - stop_index
+        
