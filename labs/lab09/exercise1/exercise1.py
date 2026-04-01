76d6de4a38beafda1af0b05e2e9e216837e5e316
ilsyafiq <syfiqiikhwan@gmail.com>
2026-04-01 12:50:29 +0800

    exercise1

--- labs/lab09/exercise1/exercise1.py
+++ labs/lab09/exercise1/exercise1.py
@@ -2,4 +2,15 @@ import pandas as pd
 
 
 def explore_data(filename):
-    pass
+    df = pd.read_csv(filename)
+
+    dict_statistics = {
+        "total_students": len(df),
+        "subjects": list(df.columns),
+        "math_average": round(float(df["Math"].mean()), 1),
+        "highest_math_student": df.loc[df["Math"].idxmax(), "Name"]
+    }
+
+    print (dict_statistics)
+
+explore_data("labs/lab09/data/students.csv")
