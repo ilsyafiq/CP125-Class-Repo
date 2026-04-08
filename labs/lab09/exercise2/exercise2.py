7f7cc4db4dbabb6c0c3c747b1423d91888ec7457
ilsyafiq <syfiqiikhwan@gmail.com>
2026-04-08 11:17:14 +0800

    changes

--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -2,4 +2,19 @@ import pandas as pd
 
 
 def compare_averages(filename):
-    pass
+    df = pd.read_csv(filename)
+
+    subject_averages = {
+        "Math": round(float(df["Math"].mean()), 1),
+        "Science": round(float(df["Science"].mean()), 1),
+        "English": round(float(df["English"].mean()), 1)
+    }
+
+    dict_averages = dict(subject_averages)
+    dict_averages["best_subject"] = max(subject_averages, key=subject_averages.get)
+    dict_averages["worst_subject"] = min(subject_averages, key=subject_averages.get)
+
+
+    print(dict_averages)
+
+compare_averages("labs/lab09/data/students.csv")
--- labs/lab09/exercise3/exercise3.py
+++ labs/lab09/exercise3/exercise3.py
@@ -3,4 +3,39 @@ import matplotlib.pyplot as plt
 
 
 def show_math_trend(filename):
-    pass
+    df = pd.read_csv(filename)
+    math_scores = df["Math"]
+
+    plt.plot(df.index, math_scores)
+    plt.xlabel("Student Index")
+    plt.ylabel("Math Score")
+    plt.title("Math Score Trends")
+    plt.show()
+
+    return len(df)
+
+'''
+Background:
+
+Show how Math scores vary across all students using a line chart.
+
+Task:
+
+Write a function show_math_trend(filename) that:
+
+Loads CSV
+Extracts Math column
+Creates line chart:
+x-axis = student index (df.index)
+y-axis = Math scores
+xlabel = "Student Index"
+ylabel = "Math Score"
+title = "Math Score Trends"
+Calls plt.show()
+Returns number of students (for testing)
+
+example
+count = show_math_trend("data/students.csv")
+# Chart window appears showing Math scores
+print(count)  # 25
+'''
